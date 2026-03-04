d299151aa169a33dafbf5a68a8de946503444351
Cade Andrasco <CMAndrasco@student.fullsail.edu>
2026-03-04 11:15:08 -0500

    Add project files.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int jumpTimesMax;
+    [SerializeField] int gravity;
+
+
+    int jumpCount;
+    int HPOriginal;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
