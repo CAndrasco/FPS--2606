bf96f3b8c4a5617fd63ee733883185009eb4505e
Cade Andrasco <CMAndrasco@student.fullsail.edu>
2026-03-10 10:30:27 -0400

    Got enemy AI to move once player is in range and do damage to player once touching

--- Assets/Scripts/enemyAI_1.cs
+++ Assets/Scripts/enemyAI_1.cs
@@ -6,17 +6,21 @@ public class enemyAI_1 : MonoBehaviour, IDamage
 {
     [Header("---- Unity Components ----")]
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] LayerMask ignoreLayer;
 
     [Header("---- Enemy Settings ----")]
     [SerializeField] int HP;
-    //[SerializeField] GameObject Weapon;
-    //[SerializeField] float damageRate;
+    [SerializeField] GameObject Weapon;
+    [SerializeField] float damageRate;
     [SerializeField] Transform armHittingPos1;
     [SerializeField] Transform armHittingPos2;
     [SerializeField] Transform armPivot1;
     [SerializeField] Transform armPivot2;
 
     Color colorOrig;
+
+    bool playerInRange;
 
     float damageTimer;
 
@@ -29,13 +33,33 @@ public class enemyAI_1 : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerInRange)
+        {
+            agent.SetDestination(gamemanager.instance.player.transform.position);
+        }
     }
 
-    //void Shoot() -- Not needed at the moment
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    //void Shoot()
     //{
     //    damageTimer = 0;
     //    Instantiate(Weapon, armHittingPos1.position, armPivot1.rotation);
+    //    Instantiate(Weapon, armHittingPos2.position, armPivot2.rotation);
     //}
 
     public void TakeDamage(int damage)
