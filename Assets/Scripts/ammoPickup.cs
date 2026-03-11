84bf85d329f65b9d0de5e80be604c647b469d8fc
Timothy Howard <scaryzombiebat@gmail.com>
2026-03-10 21:09:44 -0400

    Improved AmmoPickup trigger logic to check Player tag before adding ammo.

--- Assets/Scripts/ammoPickup.cs
+++ Assets/Scripts/ammoPickup.cs
@@ -4,14 +4,17 @@ public class ammoPickup : MonoBehaviour
 {
     [SerializeField] int ammoAmount = 1;
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        playerController player = other.GetComponent<playerController>();
-
-        if (player != null)
+        if (other.CompareTag("Player"))
         {
-            player.AddAmmo(ammoAmount);
-            Destroy(gameObject);
+            playerController player = other.GetComponent<playerController>();
+
+            if (player != null ) 
+            {
+                player.AddAmmo(ammoAmount);
+                Destroy(gameObject);
+            }
         }
     }
 }
